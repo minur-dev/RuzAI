ef456f746720db0e685b7cb5873315f7737b2c09
minur-dev <72206117+minur-dev@users.noreply.github.com>
2021-11-14 14:04:39 +0530

    Add files via upload

---
+++ ConsoleApprp/ConsoleApprp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApprp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello!");
+            string v = Console.ReadLine();
+            metod();
+        }
+        private static void metod()
+        {
+
+            Console.WriteLine("hola");
+
+            metod();
+        }
+        
+
+
+    
+
+
+
+}
